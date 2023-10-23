2472e9f5f8030ddc3790f3ae1fb39ffaa481cffb
Laurenda Edorh <laurenda98@gmail.com>
2023-10-23 13:08:07 +0200

    la premiere partie de la video P1

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini-ERP</title>
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><i class="fas fa-user-secret"></i> Coding City</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <!----------
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>----->
+
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section class="container py-5">
+        <div class="row">
+            <div class="col-lg-8 col-sm mb-5 mx-auto">
+                <h1 class="fs-4 text-center lead text-primary">CRUD PHP POO MYSQL AJAX</h1>
+            </div>
+        </div>
+
+        <div class="dropdown-divider border-warning"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <h5 class="fw-bold mb-0">Liste des factures</h5>
+            </div>
+            <div class="col-md-6">
+
+                <div class="d-flex justify-content-end">
+                    <button class="btn btn-primary btn-sm me-3"><i class="fas fa-folder-plus"></i> Nouveau</button>
+                    <a href="#" class="btn btn-success btn-sm" id="export"><i class="fas fa-table"></i>Exporter</a>
+                </div>
+            </div>
+        </div>
+        <div class="dropdown-divider border-warning"></div>
+        <div class="row">
+            <div class="table-responsive" id="orderTable">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Client</th>
+                            <th scope="col">Caissier</th>
+                            <th scope="col">Montant</th>
+                            <th scope="col">Perçu</th>
+                            <th scope="col">Retourné</th>
+                            <th scope="col">Etat</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        for ($i = 1; $i < 100; $i++) : ?>
+
+                            <tr>
+                                <th scope="row"><?= $i ?></th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+
+                                <td>
+                                    <a href="#" class="text-info me-2 infoBtn" title="Voir details"><i class="fas fa-info-circle"></i></a>
+                                    <a href="#" class="text-primary me-2 infoBtn" title="Modifier"><i class="fas fa-edit"></i></a>
+                                    <a href="#" class="text-danger me-2 infoBtn" title="Supprimer"><i class="fas fa-trash-alt"></i></a>
+
+                                </td>
+
+                            </tr>
+                    </tbody>
+                <?php endfor; ?>
+                </table>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="process.js"></script>
+
+
+
+</body>
+
+</html>
