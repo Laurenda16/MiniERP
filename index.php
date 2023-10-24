6acdc135e15b4af4543c038456c6a5f51da15c3e
Laurenda Edorh <laurenda98@gmail.com>
2023-10-24 05:42:00 +0200

    premiere partie

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini-ERP</title>
     <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.css" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -80,7 +81,7 @@
                     </thead>
                     <tbody>
                         <?php
-                        for ($i = 1; $i < 100; $i++) : ?>
+                        for ($i = 1; $i < 50; $i++) : ?>
 
                             <tr>
                                 <th scope="row"><?= $i ?></th>
